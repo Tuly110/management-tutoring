11434ced12fe5cb10e3240b093ff5bb98e8ed1df
Ly <tytly0110@gmail.com>
2024-07-31 06:05:35 +0700

    edit profile parent and student

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -34,7 +34,16 @@ class UserController extends Controller
     {
         $data['getRecord']= User::getSingle(Auth::user()->id);
         $data['header_title']= "My account";
-        return view('teacher.my_account', $data);
+        if(Auth::user()->usertype==2)
+        {
+            return view('teacher.my_account', $data);
+        }else if(Auth::user()->usertype==3)
+        {
+            return view('student.my_account', $data);
+        }else{
+            return view('parent.my_account', $data);
+        }
+        
     }
     public function update_my_account(Request $request)
     {
@@ -76,6 +85,80 @@ class UserController extends Controller
  
          }
         $teacher->save();
+        return redirect()->back()-> with('success', "Account updated successfully");
+    }
+
+    public function update_my_account_student(Request $request)
+    {
+        // dd($request->all());
+        $id = Auth::user()->id;
+        $student = User::getSingle($id);
+        $request->validate([
+            'email' => 'required|email|unique:users,email,'.$student->id,
+            'mobile_number' => 'max:15|min:8',
+        ]);
+        $student->name = trim($request->first_name);
+        $student->last_name = trim($request->last_name);
+        $student->mobile_number = trim($request->mobile_number);
+        $student->gender = trim($request->gender);
+        $student->email = trim($request->email);
+        $student->date_of_birth = trim($request->date_of_birth);
+        $student->caste = trim($request->caste);
+        $student->religion = trim($request->religion);
+        $student->blood_group = trim($request->blood_group);
+        $student->height = trim($request->height);
+        $student->weight = trim($request->weight);
+         // upload anh
+         if(!empty($request->file('profile_pic')))
+         {
+             if(!empty($student->getProfile()))
+             {
+                 unlink('upload/profile/'.$student->profile_pic);
+             }
+             $ext= $request->file('profile_pic')->getClientOriginalExtension();
+             $file= $request->file('profile_pic');
+             $randomStr = date('Ymdis').Str::random(20);
+             $filename = strtolower($randomStr).'.'.$ext;
+             $file->move('upload/profile/',$filename);
+             $student->profile_pic =  $filename;
+ 
+         }
+        $student->save();
+        return redirect()->back()-> with('success', "Account updated successfully");
+    }
+    public function update_my_account_parent(Request $request)
+    {
+        // dd($request->all());
+        $id = Auth::user()->id;
+        $parent = User::getSingle($id);
+        $request->validate([
+            'email' => 'required|email|unique:users,email,'.$parent->id,
+            'address'=>'max:255',
+            'occupation'=>'max:255',
+            'mobile_number' => 'max:15|min:8',
+        ]);
+        $parent->name = trim($request->first_name);
+        $parent->last_name = trim($request->last_name);
+        $parent->mobile_number = trim($request->mobile_number);
+        $parent->email = trim($request->email);
+        $parent->occupation = trim($request->occupation);
+        $parent->address = trim($request->address);
+         // upload anh
+         if(!empty($request->file('profile_pic')))
+         {
+             if(!empty($parent->getProfile()))
+             {
+                 unlink('upload/profile/'.$parent->profile_pic);
+             }
+             $ext= $request->file('profile_pic')->getClientOriginalExtension();
+             $file= $request->file('profile_pic');
+             $randomStr = date('Ymdis').Str::random(20);
+             $filename = strtolower($randomStr).'.'.$ext;
+             $file->move('upload/profile/',$filename);
+             $parent->profile_pic =  $filename;
+ 
+         }
+        $parent->save();
         return redirect()->back()-> with('success', "Account updated successfully");
     }
 }
--- resources/views/parent/my_account.blade.php
+++ resources/views/parent/my_account.blade.php
@@ -3,8 +3,10 @@
 <main id="main" class="main">
 
     <div class="pagetitle">
-      <h1>Edit Parent</h1>
+      <h1>My account</h1>
     </div><!-- End Page Title -->
+    @include('_message')
+
     <section class="section">
       <div class="row">
         <div class="col-12">
@@ -34,15 +36,13 @@
                     <div class="text-danger"><b>{{ $errors->first('mobile_number') }}</b></div>
                   </div>
                   <div class="col-6">
-                    <label class="form-label">Gender<span style="color: red">*</span></label>
-                    <select class="form-control" name="gender" id="" required> 
-                      <option value="">Select gender</option>
-                      <option {{ (old('gender', $getRecord->gender) == 'Male') ? 'selected' : ''  }} value="Male">Male</option>
-                      <option {{ (old('gender', $getRecord->gender) == 'Female') ? 'selected' : ''  }} value="Female">Female</option>
-                      <option {{ (old('gender', $getRecord->gender) == 'Other') ? 'selected' : ''  }} value="Other">Other</option>
-                    </select>
-                    <div class="text-danger"><b>{{ $errors->first('gender') }}</b></div>
-                  </div>
+                    <label class="form-label">Profile pic<span style="color: red">*</span></label>
+                    <input type="file" class="form-control" value="{{ old('profile_pic') }}" name="profile_pic" >
+                    <div class="text-danger"><b>{{ $errors->first('profile_pic') }}</b></div>
+                    @if (!empty($getRecord->getProfile()))
+                        <img src="{{ $getRecord->getProfile() }}" alt="" style="width: 100px">
+                    @endif
+                </div>
                 </div>
                 <div class="row mt-4">
                     <div class="col-6">
@@ -56,34 +56,11 @@
                       <div class="text-danger"><b>{{ $errors->first('address') }}</b></div>
                     </div>
                   </div>
-                <div class="row mt-4">
-                    <div class="col-6">
-                        <label for="inputNanme4" class="form-label">Status</label>
-                        <select class="form-control" name="status" required>
-                            <option {{ (old('status', $getRecord->status) == 0) ? 'selected' : ''  }} value="0">Active</option>
-                            <option {{ (old('status', $getRecord->status) == 1) ? 'selected' : ''  }} value="1">InActive</option>
-                        </select>
-                    </div>
-                    <div class="col-6">
-                        <label class="form-label">Profile pic<span style="color: red">*</span></label>
-                        <input type="file" class="form-control" value="{{ old('profile_pic') }}" name="profile_pic" >
-                        <div class="text-danger"><b>{{ $errors->first('profile_pic') }}</b></div>
-                        @if (!empty($getRecord->getProfile()))
-                            <img src="{{ $getRecord->getProfile() }}" alt="" style="width: 100px">
-                        @endif
-                    </div>
-                </div>
                 <div class="col-12">
                   <label class="form-label">Email<span style="color: red">*</span></label>
                   <input type="email" class="form-control" value="{{ old('email', $getRecord->email) }}" name="email" required>
                   <div class="text-danger"><b>{{ $errors->first('email') }}</b></div>
                 </div>
-                <div class="col-12">
-                  <label for="inputPassword4" class="form-label">Password<span style="color: red">*</span></label>
-                  <input type="password" class="form-control" id="inputPassword4" name="password" >
-                  <p class="text-primary">Do you want change your password?</p>
-                </div>
-
                 <div class="text-center">
                   <button type="submit" class="btn btn-primary" name="add" >Update</button>
                 </div>
